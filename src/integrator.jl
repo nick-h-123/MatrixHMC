027bbea00eb9544ad52f36bc62199d923685ce40
nick-h-123 <nicholasdheller@gmail.com>
2021-05-26 13:10:32 -0700

    Update integrator.jl

--- src/integrator.jl
+++ src/integrator.jl
@@ -55,8 +55,29 @@ stat(lf::AbstractLeapfrog) = (step_size=step_size(lf), nom_step_size=nom_step_si
 
 update_nom_step_size(lf::AbstractLeapfrog, ϵ) = reconstruct(lf, ϵ=ϵ)
 
+"""
+$(TYPEDEF)
+
+Leapfrog integrator with fixed step size `ϵ`.
+
+# Fields
+
+$(TYPEDFIELDS)
+"""
+struct Leapfrog{T<:AbstractScalarOrVec{<:AbstractFloat}} <: AbstractLeapfrog{T}
+    "Step size."
+    ϵ       ::  T
+end
+Base.show(io::IO, l::Leapfrog) = print(io, "Leapfrog(ϵ=$(round.(l.ϵ; sigdigits=3)))")
+
+struct MTLeapfrog{T<:AbstractScalarOrVec{<:AbstractFloat}} <: AbstractLeapfrog{T}
+    "Step size."
+    ϵ       ::  T
+end
+Base.show(io::IO, l::MTLeapfrog) = print(io, "Leapfrog(ϵ=$(round.(l.ϵ; sigdigits=3)))")
+
 function step(
-    lf::AbstractLeapfrog{T},
+    lf::Leapfrog{T},
     h::Hamiltonian,
     z::P,
     n_steps::Int=1;
@@ -108,17 +129,55 @@ function step(
     return res
 end
 
-"""
-$(TYPEDEF)
+function step(
+    lf::MTLeapfrog{T},
+    h::Hamiltonian,
+    z::P,
+    n_steps::Int=1;
+    fwd::Bool=n_steps > 0,  # simulate hamiltonian backward when n_steps < 0
+    full_trajectory::Val{FullTraj} = Val(false)
+) where {T<:AbstractScalarOrVec{<:AbstractFloat}, P<:PhasePoint, FullTraj}
+    n_steps = abs(n_steps)  # to support `n_steps < 0` cases
 
-Leapfrog integrator with fixed step size `ϵ`.
+    ϵ = fwd ? step_size(lf) : -step_size(lf)
+    #ϵ = ϵ'
 
-# Fields
+    res = if FullTraj
+        Vector{P}(undef, n_steps)
+    else
+        z
+    end
 
-$(TYPEDFIELDS)
-"""
-struct Leapfrog{T<:AbstractScalarOrVec{<:AbstractFloat}} <: AbstractLeapfrog{T}
-    "Step size."
-    ϵ       ::  T
+    @unpack θ, r = z
+    @unpack value, gradient = z.ℓπ
+    for i = 1:n_steps
+        # Tempering
+        r = temper(lf, r, (i=i, is_half=true), n_steps)
+        # Take a half leapfrog step for momentum variable
+        r = r - ϵ / 2 .* gradient
+        # Take a full leapfrog step for position variable
+        ∇r = ∂H∂r(h, r)
+        θ = θ + ϵ .* ∇r
+        # Take a half leapfrog step for momentum variable
+        @unpack value, gradient = ∂H∂θ(h, θ)
+        r = r - ϵ / 2 .* gradient
+        # Tempering
+        r = temper(lf, r, (i=i, is_half=false), n_steps)
+        # Create a new phase point by caching the logdensity and gradient
+        z = phasepoint(h, θ, r; ℓπ=DualValue(value, gradient))
+        # Update result
+        if FullTraj
+            res[i] = z
+        else
+            res = z
+        end
+        if !isfinite(z)
+            # Remove undef
+            if FullTraj
+                res = res[isassigned.(Ref(res), 1:n_steps)]
+            end
+            break
+        end
+    end
+    return res
 end
-Base.show(io::IO, l::Leapfrog) = print(io, "Leapfrog(ϵ=$(round.(l.ϵ; sigdigits=3)))")
